0eef3f3bd35aa47c0a920cc757a0e8d4c1ab3d3d
Carlos de la Cruz <carlosdelacruzacosta4@gmail.com>
2021-02-10 19:42:20 +0000

    testing html/css

--- server.js
+++ server.js
@@ -1,8 +1,15 @@
 const express = require('express');
+const path = require('path');
 const app = express();
+
+app.use(express.static('views'));
 
 app.get('/', (req, res) => {
   res.send("Sup");
+});
+
+app.get('/login', (req, res) => {
+  res.sendFile(path.join(__dirname + '/views/login.html'));
 });
 
 app.listen(3000, () => console.log("Yo, it's up"));
